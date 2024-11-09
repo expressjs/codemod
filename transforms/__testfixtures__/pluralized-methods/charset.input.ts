c2a8346cd626a4744065795561f5f072955ac556
Sebastian Beltran <bjohansebas@gmail.com>
2024-11-09 11:04:15 -0500

    add some tests (#11)

--- transforms/__testfixtures__/pluralized-methods/charset.input.ts
+++ transforms/__testfixtures__/pluralized-methods/charset.input.ts
@@ -4,6 +4,10 @@ const app = express()
 
 app.get('/', (req, res) => {
     const charset = req.acceptsCharset('utf-8');
-    res.json({ charset }, 200);
+    res.json({ charset });
 });
 
+app.get('/', function (request, response) {
+    const charset = request.acceptsCharset('utf-8');
+    response.json({ charset });
+});
--- transforms/__testfixtures__/redirect/redirect.input.ts
+++ transforms/__testfixtures__/redirect/redirect.input.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import { redirect } from "somelibrary";
 
 const app = express();
 
 app.get("/", function (req, res) {
   res.redirect("/other-page", 301);
+});
+
+app.get("/", function (req, response) {
+  response.redirect("/other-page", 301);
+});
+
+app.get("/", function (req, res) {
+  redirect(301, "/other-page");
 });
