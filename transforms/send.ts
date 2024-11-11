cda53a33e7ab02f2ab4adb887979a405fbd58097
Filip Kudła <kjugi@users.noreply.github.com>
2024-11-11 01:55:35 +0100

    base idea to solve single param .send method

--- transforms/send.ts
+++ transforms/send.ts
@@ -1,5 +1,5 @@
 import type { API, FileInfo } from 'jscodeshift'
-import { CallExpression, withParser } from 'jscodeshift'
+import { CallExpression, callExpression, identifier, memberExpression, withParser } from 'jscodeshift'
 
 export default function transformer(file: FileInfo, _api: API): string {
   const parser = withParser('ts')
@@ -13,8 +13,25 @@ export default function transformer(file: FileInfo, _api: API): string {
           },
         },
       })
+      .map((path) => {
+        // from v3: https://expressjs.com/en/3x/api.html#res.send
+        const pathArguments = path.node.arguments
+        if (pathArguments.length === 2) {
+          const statusParamIndex = pathArguments.findIndex((arg) => arg.type === 'NumericLiteral')
+          const bodyParamIndex = pathArguments.findIndex((arg) => arg.type !== 'NumericLiteral')
+
+          // TODO
+        } else if (pathArguments.length === 1) {
+          const statusValue = pathArguments[0].type === 'Identifier' ? pathArguments[0].name : false
+          path.value.callee = callExpression(
+            memberExpression(identifier('res'), identifier('sendStatus')),
+            statusValue ? [identifier(statusValue)] : [],
+          )
+        }
+
+        return path
+      })
       // TODO: res.send(status, body) and res.send(body, status) signatures: Use res.status(status).send(body).
-      // TODO: res.send(status) signature: Use res.sendStatus(status) for simple status responses, or res.status(status).send() for sending a status code with an optional body.
       .toSource()
   )
 }
