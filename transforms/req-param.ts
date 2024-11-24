75f33dc7ddc43dd8a77766525382d5a172162e91
Filip Kudła <11212541+kjugi@users.noreply.github.com>
2024-11-24 21:38:28 +0100

    feat: req.param transform (#5)
    * start transform for req.param method to object
    
    * replace callExpression with memberExpression for req.param
    
    * add tests for req.param nested object keys
    
    * fix lint
    
    ---------
    
    Co-authored-by: Filip Kudła <kjugi@users.noreply.github.com>

---
+++ transforms/req-param.ts
@@ -0,0 +1,44 @@
+import type { API, FileInfo } from 'jscodeshift'
+import { CallExpression, identifier, memberExpression, withParser } from 'jscodeshift'
+import { recursiveParent } from '../utils/recursiveParent'
+
+export default function transformer(file: FileInfo, _api: API): string {
+  const parser = withParser('ts')
+
+  return parser(file.source)
+    .find(CallExpression, {
+      callee: {
+        property: {
+          name: 'param',
+        },
+      },
+    })
+    .filter((path) => Boolean(recursiveParent(path.parentPath)))
+    .replaceWith((path) => {
+      const pathArguments = path.node.arguments
+
+      if (pathArguments.length > 1) {
+        return path
+      }
+
+      if (pathArguments[0].type === 'StringLiteral') {
+        if (pathArguments[0].value === 'query') {
+          // convert to req.query
+          return memberExpression(identifier(recursiveParent(path.parentPath) || 'req'), identifier('query'))
+        }
+        if (pathArguments[0].value === 'body') {
+          // convert to req.body
+          return memberExpression(identifier(recursiveParent(path.parentPath) || 'req'), identifier('body'))
+        }
+
+        // convert to req.params.[value]
+        return memberExpression(
+          memberExpression(identifier(recursiveParent(path.parentPath) || 'req'), identifier('params')),
+          identifier(pathArguments[0].value),
+        )
+      }
+
+      return path
+    })
+    .toSource()
+}
