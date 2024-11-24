352dde64850c49fbce5bf8710d08492b1f642356
Filip Kudła <kjugi@users.noreply.github.com>
2024-11-24 20:25:32 +0100

    replace callExpression with memberExpression for req.param

--- transforms/req-param.ts
+++ transforms/req-param.ts
@@ -1,43 +1,44 @@
 import type { API, FileInfo } from 'jscodeshift'
-import { CallExpression, memberExpression, withParser } from 'jscodeshift'
+import { CallExpression, identifier, memberExpression, withParser } from 'jscodeshift'
 import { recursiveParent } from '../utils/recursiveParent'
 
 export default function transformer(file: FileInfo, _api: API): string {
   const parser = withParser('ts')
 
-  return (
-    parser(file.source)
-      .find(CallExpression, {
-        callee: {
-          property: {
-            name: 'param',
-          },
+  return parser(file.source)
+    .find(CallExpression, {
+      callee: {
+        property: {
+          name: 'param',
         },
-      })
-      .filter((path) => Boolean(recursiveParent(path.parentPath)))
-      .map((path) => {
-        console.log(path.node.arguments)
-        const pathArguments = path.node.arguments
+      },
+    })
+    .filter((path) => Boolean(recursiveParent(path.parentPath)))
+    .replaceWith((path) => {
+      const pathArguments = path.node.arguments
 
-        if (pathArguments.length > 1) {
-          return path
+      if (pathArguments.length > 1) {
+        return path
+      }
+
+      if (pathArguments[0].type === 'StringLiteral') {
+        if (pathArguments[0].value === 'query') {
+          // convert to req.query
+          return memberExpression(identifier(recursiveParent(path.parentPath) || 'req'), identifier('query'))
+        }
+        if (pathArguments[0].value === 'body') {
+          // convert to req.body
+          return memberExpression(identifier(recursiveParent(path.parentPath) || 'req'), identifier('body'))
         }
 
-        if (pathArguments[0].type === 'StringLiteral') {
-          if (pathArguments[0].value === 'query') {
-            // convert to req.query
-            return memberExpression()
-          }
-          if (pathArguments[0].value === 'body') {
-            // convert to req.body
-            return memberExpression()
-          }
+        // convert to req.params.[value]
+        return memberExpression(
+          memberExpression(identifier(recursiveParent(path.parentPath) || 'req'), identifier('params')),
+          identifier(pathArguments[0].value),
+        )
+      }
 
-          // convert to req.params.[value]
-          return memberExpression()
-        }
-      })
-      // TODO: req.param(name): This method has been deprecated. Instead, access parameters directly via req.params, or use req.body or req.query as needed.
-      .toSource()
-  )
+      return path
+    })
+    .toSource()
 }
