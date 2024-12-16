6d0d8fa33d3681bff3f560620c5757d35e6912c0
Sebastian Beltran <bjohansebas@gmail.com>
2024-12-16 10:23:36 -0500

    reduce try/catch

--- commands/upgrade.ts
+++ commands/upgrade.ts
@@ -17,67 +17,11 @@ export async function upgrade(source?: string) {
     process.exit(1)
   }
 
+  let packageJson = ''
+
   try {
     const packageJsonPath = resolve(sourceSelected || '', 'package.json')
-    const packageJson = JSON.parse(await readFile(packageJsonPath, 'utf8'))
-
-    const codemods = suggestCodemods(packageJson)
-
-    if (codemods.length === 0) {
-      console.info('> No codemods are suggested for this project. \n')
-
-      return
-    }
-
-    const { codemodsSelected } = await prompts(
-      {
-        type: 'multiselect',
-        name: 'codemodsSelected',
-        message: `The following 'codemods' are recommended for your upgrade. Select the ones to apply.`,
-        choices: codemods.map(({ description, value, version }) => {
-          return {
-            title: `(v${version}) ${value}`,
-            description,
-            value,
-            selected: true,
-          }
-        }),
-      },
-      { onCancel },
-    )
-
-    const args: Options = {
-      dry: false,
-      babel: false,
-      silent: true,
-      ignorePattern: '**/node_modules/**',
-      extensions: 'cts,mts,ts,js,mjs,cjs',
-    }
-
-    const results = {
-      ok: 0,
-      skipped: 0,
-      failed: 0,
-      unmodified: 0,
-    }
-
-    for (const codemod of codemodsSelected) {
-      const transformerPath = require.resolve(`${transformerDirectory}/${codemod}.js`)
-
-      console.log(`> Applying codemod: ${codemod}`)
-      const { ok, skip, error, nochange } = await jscodeshift(transformerPath, [resolve(sourceSelected)], args)
-
-      results.ok += ok
-      results.skipped += skip
-      results.failed += error
-      results.unmodified += nochange
-    }
-
-    console.log('\n> Summary of the upgrade')
-    console.log(`> ${results.ok} codemods were applied successfully`)
-    console.log(`> ${results.skipped} codemods were skipped`)
-    console.log(`> ${results.failed} codemods failed`)
-    console.log(`> ${results.unmodified} codemods were skipped because they didn't change anything`)
+    packageJson = JSON.parse(await readFile(packageJsonPath, 'utf8'))
   } catch (err) {
     if (err.code === 'ENOENT') {
       console.info('> No package.json found in the selected directory. \n')
@@ -86,6 +30,64 @@ export async function upgrade(source?: string) {
       console.error(err.message)
     }
   }
+
+  const codemods = suggestCodemods(packageJson)
+
+  if (codemods.length === 0) {
+    console.info('> No codemods are suggested for this project. \n')
+
+    return
+  }
+
+  const { codemodsSelected } = await prompts(
+    {
+      type: 'multiselect',
+      name: 'codemodsSelected',
+      message: `The following 'codemods' are recommended for your upgrade. Select the ones to apply.`,
+      choices: codemods.map(({ description, value, version }) => {
+        return {
+          title: `(v${version}) ${value}`,
+          description,
+          value,
+          selected: true,
+        }
+      }),
+    },
+    { onCancel },
+  )
+
+  const args: Options = {
+    dry: false,
+    babel: false,
+    silent: true,
+    ignorePattern: '**/node_modules/**',
+    extensions: 'cts,mts,ts,js,mjs,cjs',
+  }
+
+  const results = {
+    ok: 0,
+    skipped: 0,
+    failed: 0,
+    unmodified: 0,
+  }
+
+  for (const codemod of codemodsSelected) {
+    const transformerPath = require.resolve(`${transformerDirectory}/${codemod}.js`)
+
+    console.log(`> Applying codemod: ${codemod}`)
+    const { ok, skip, error, nochange } = await jscodeshift(transformerPath, [resolve(sourceSelected)], args)
+
+    results.ok += ok
+    results.skipped += skip
+    results.failed += error
+    results.unmodified += nochange
+  }
+
+  console.log('\n> Summary of the upgrade')
+  console.log(`> ${results.ok} codemods were applied successfully`)
+  console.log(`> ${results.skipped} codemods were skipped`)
+  console.log(`> ${results.failed} codemods failed`)
+  console.log(`> ${results.unmodified} codemods were skipped because they didn't change anything`)
 }
 
 function suggestCodemods(packageJson) {
