4cb2b1a80a11af8de2beb1999248c7e15877500b
Sebastian Beltran <bjohansebas@gmail.com>
2025-12-23 22:09:12 -0500

    feat(req-param): migrate req-param to codemod.com

---
+++ codemods/req-param/src/workflow.ts
@@ -0,0 +1,75 @@
+import type Js from '@codemod.com/jssg-types/src/langs/javascript'
+import type { Edit, SgNode, SgRoot } from '@codemod.com/jssg-types/src/main'
+
+function getStringLiteralValue(node: SgNode<Js>): string | null {
+  if (!node.is('string')) return null
+
+  const fragments = node.findAll({ rule: { kind: 'string_fragment' } })
+  if (fragments.length !== 1) return null
+  return fragments[0]?.text() ?? null
+}
+
+function findParentFunctionParameters(node: SgNode<Js>): SgNode<Js, 'formal_parameters'> | null {
+  let parent = node.parent()
+  while (parent) {
+    if (parent.is('formal_parameters')) return parent
+    parent = parent.parent()
+  }
+  return null
+}
+
+async function transform(root: SgRoot<Js>): Promise<string | null> {
+  const rootNode = root.root()
+
+  const nodes = rootNode.findAll({
+    rule: {    
+      pattern: '$OBJ.$METHOD($ARG)',
+    },
+    constraints: {
+      METHOD: { regex: '^(param)$' },
+    },
+  })
+
+  const edits: Edit[] = []
+
+  for (const call of nodes) {
+    const arg = call.getMatch('ARG')
+    const obj = call.getMatch('OBJ')
+    
+    if (!arg || !obj) continue
+    const objDef = obj.definition({ resolveExternal: false })
+    if (!objDef) continue
+
+    const isParameter = objDef.node.matches({
+      rule: { inside: { kind: 'formal_parameters', stopBy: 'end' } },
+    })
+    if (!isParameter) continue
+
+    const parameters = findParentFunctionParameters(objDef.node)
+    if (!parameters) continue
+    const firstParameter = parameters.find({ rule: { kind: 'identifier' } })
+
+    if (!firstParameter) continue
+    const requestName = firstParameter.text()
+
+    const argValue = getStringLiteralValue(arg)
+
+    if (argValue === "body") {
+      edits.push(call.replace(`${requestName}.body`))
+    }
+
+    if(argValue === "query") {
+      edits.push(call.replace(`${requestName}.query`))
+    }
+
+    if(argValue) {
+      edits.push(call.replace(`${requestName}.params.${argValue}`))
+    }
+  }
+
+  if (edits.length === 0) return null
+
+  return rootNode.commitEdits(edits)
+}
+
+export default transform
