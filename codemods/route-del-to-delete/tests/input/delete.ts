ce5c9adfe7098463254c74bb1e607d16c27155b0
Sebastian Beltran <bjohansebas@gmail.com>
2026-01-13 09:59:35 -0500

    feat(route-del-to-delete): add migration codemod for app.del() to app.delete() (#102)
    * feat(route-del-to-delete): add migration codemod for app.del() to app.delete()
    
    * fix(workflow): simplify early return checks for nodes and edits
    
    * fix(codemod): update repository URL in codemod.yaml
    
    * fix(docs): clarify deprecation of app.del() in migration guide

---
+++ codemods/route-del-to-delete/tests/input/delete.ts
@@ -0,0 +1,39 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {});
+
+app.del([""],() => {
+  myImportantLogic();
+});
+
+app.del([],() => {
+  myImportantLogic();
+});
+
+app.del(/d/,() => {
+  myImportantLogic();
+});
+
+app.del(() => {
+  myImportantLogic();
+});
+
+app.del(function () {
+  myImportantLogic();
+});
+
+app.del("/old", () => {
+  myImportantLogic();
+});
+
+function noModify() {
+  let a
+
+  app.del(a)
+}
+
+const myImportantLogic = () => {
+  console.log("making sure it's there");
+};
